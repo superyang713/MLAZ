4a0e935b2409f0cfbc1dedcd0f7c8deba82d8193
Yang Dai <yangdai713@aim.com>
2018-09-18 16:20:35 -0400

    support vector regression

---
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -0,0 +1,51 @@
+# SVR
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values.reshape(-1, 1)
+
+
+# Feature Scaling --> it is not included in SVR class.
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Fitting SVR to the dataset
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
+
+# Visualising the SVR results
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result
+y_pred = regressor.predict(sc_X.transform(6.5))
+y_pred = sc_y.inverse_transform(y_pred)
+print(y_pred)
+
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+# choice of 0.01 instead of 0.1 step because the data is feature scaled
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
